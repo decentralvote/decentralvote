228b08b8fab1ecc92de86756def21e0393e274c9
bighappyface <anomalous20@gmail.com>
2021-07-20 15:33:50 -0400

    feat(deploy): add dotenv and testnets to hardhat

--- scripts/deploy-decentralpoll.js
+++ scripts/deploy-decentralpoll.js
@@ -1,4 +1,4 @@
-const hre = require("hardhat");
+const hardhat = require("hardhat");
 
 async function main() {
   // Membership Protocol Addresses
@@ -6,27 +6,28 @@ async function main() {
 
   // Proposal Name Array
   const proposalNames = [
-    ethers.utils.formatBytes32String("DV Proposal 1"),
-    ethers.utils.formatBytes32String("DV Proposal 2")
+    ethers.utils.formatBytes32String("Obama"),
+    ethers.utils.formatBytes32String("Romney")
   ];
 
   // Voter Base Logic
-  const voterBaseLogic = ethers.utils.formatBytes32String("DV Voter Base Logic");
+  const voterBaseLogic = ethers.utils.formatBytes32String("National Voting");
 
   // Poll Name
-  const pollName = ethers.utils.formatBytes32String("DV Poll Name");
+  const pollName = ethers.utils.formatBytes32String("2012 US Presidential Election");
 
   // Poll Type
-  const pollType = ethers.utils.formatBytes32String("DV Poll Type");
+  const pollType = ethers.utils.formatBytes32String("One Person One Vote");
 
   // Start Time
+  // await ethers.provider.send("evm_mine", []);
   const presentTime = (await ethers.provider.getBlock()).timestamp;
   const startTime = presentTime + 60;
 
   // Duration
-  const duration = 1000;
+  const duration = 60 * 60 * 24 * 5;
   // We get the contract to deploy
-  const DecentralPollContract = await hre.ethers.getContractFactory("DecentralPoll");
+  const DecentralPollContract = await hardhat.ethers.getContractFactory("DecentralPoll");
 
   const DecentralPoll = await DecentralPollContract.deploy(
     protocolAddresses,
