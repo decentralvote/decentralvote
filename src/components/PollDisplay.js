d601f435c0b51c2522ebb19988714ad44571a7f5
SamuelRandall <samuelrandall999@gmail.com>
2021-08-02 16:32:36 -0500

    restyles and formats poll details page, fixes radio buttons

--- src/components/PollDisplay.js
+++ src/components/PollDisplay.js
@@ -1,13 +1,11 @@
 import React, {useState} from 'react';
-import Paper from "@material-ui/core/Paper";
-import Typography from "@material-ui/core/Typography";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
 import FormControl from "@material-ui/core/FormControl";
 import FormLabel from "@material-ui/core/FormLabel";
-import RadioGroup from "@material-ui/core/RadioGroup";
 import Button from "@material-ui/core/Button";
 import VoteCounts from "./VoteCounts";
 import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Radio from "@material-ui/core/Radio";
 import {ethers} from "ethers";
 import {Alert} from "@material-ui/lab";
 
@@ -22,34 +20,53 @@ function PollDisplay(props) {
   function ProposalList(props) {
     const proposals = props.proposals;
     return proposals.map((proposal, index) =>
-      <FormControlLabel checked={selectedVote === index} value={selectedVote} onChange={e => setSelectedVote(e.target.checked)} value={index} control={<Radio />} label={ethers.utils.parseBytes32String(proposal)} />
+      <FormControlLabel value={index.toString()}
+                        control={<Radio color={"default"}/>}
+                        label={ethers.utils.parseBytes32String(proposal)}/>
     );
+  }
 
+  const handleChange = (e) => {
+    setSelectedVote(e.target.value);
+  }
   return (
-    <div>
+    <>
       {!props.instance.canVote &&
-        <Alert severity={"error"}>You are not eligible to vote in this poll.</Alert>
+      <Alert severity={"error"}>You are not eligible to vote in this
+        poll.</Alert>
       }
       {props.instance.canVote &&
-        <div>
-          <h1>{props.instance.pollName}</h1>
-          <h3 style={{color: !props.instance.hasPollEnded ? "green" : "red" }}>{!props.instance.hasPollEnded ? "Active" : "Ended"}</h3>
+      <>
+        <h1>{props.instance.pollName}</h1>
+        <h3
+          style={{color: !props.instance.hasPollEnded ? "green" : "red"}}>{!props.instance.hasPollEnded ? "Active" : "Ended"}</h3>
+        <>
+          <h4>Started: {props.instance.startTime.toString()}</h4>
+          <h4>{!props.instance.hasPollEnded ? "Ends" : "Ended"}: {props.instance.endTime.toString()}</h4>
+        </>
 
+        {props.instance.canVote &&
+        <>
           <FormControl component="fieldset">
-            <h2>Proposals:</h2>
+            <FormLabel component="legend">Proposals</FormLabel>
             <RadioGroup
               aria-label="proposals"
-              name="proposals">
-              <ProposalList proposals={props.instance.proposals} />
+              name="proposals"
+              value={selectedVote}
+              onChange={handleChange}>
+              <ProposalList proposals={props.instance.proposals}/>
+              <Button variant="contained" color="primary">
+                Vote!
+              </Button>
             </RadioGroup>
           </FormControl>
-          <Button variant="contained" color="primary">
-            Vote!
-          </Button>
-          <VoteCounts counts={props.instance.voterCounts} proposals={props.instance.proposals} />
-        </div>
+          <VoteCounts counts={props.instance.voterCounts}
+                      proposals={props.instance.proposals}/>
+        </>
+        }
+      </>
       }
-    </div>
+    </>
   );
 }
 
--- src/components/VoteCounts.js
+++ src/components/VoteCounts.js
@@ -2,30 +2,40 @@ import { ethers } from 'ethers';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-import Typography from '@material-ui/core/Typography';
+import {makeStyles} from "@material-ui/core/styles";
 
 function ListItemLink(props) {
     return <ListItem button component="a" {...props} />;
 }
 
+const useStyles = makeStyles((theme) => ({
+  results: {
+    marginBottom: 0,
+  }
+}));
+
 function VoteCounts(props) {
     let counts = props.counts;
     let proposals = props.proposals;
+
+    const classes = useStyles();
+
     let items = counts.map((count, index) => {
         let label = `${ethers.utils.parseBytes32String(proposals[index])}`;
         let votes = `Votes: ${count.toString()}`;
         return <ListItemLink>
             <ListItemText primary={label} secondary={votes} />
         </ListItemLink>
+
     });
     return <div>
-        <Typography component="h1" variant="h5">
-            Results
-        </Typography>
-        <List component="nav" aria-label="secondary mailbox folders">
-            {items}
-        </List>
-    </div>
+            <h2 className={classes.results}>
+                Results
+            </h2>
+            <List component="nav" aria-label="secondary mailbox folders">
+                {items}
+            </List>
+          </div>
 }
 
 export default VoteCounts;
