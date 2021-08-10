753727c5096d9bc62b149336a060b22e3c31a53d
SamuelRandall <samuelrandall999@gmail.com>
2021-08-10 17:46:35 -0500

    makes example types more useful

--- src/components/pollTypes.ts
+++ src/components/pollTypes.ts
@@ -1,11 +1,22 @@
+
 export type pollDisplayProps = {
-  proposals: any,
-  instance: any,
-  selectVote: any,
+  instance: instanceType,
+  selectVote(value: any): () => any,
   selectedVote: string,
-  onNext: any,
+  onNext(): () => void,
 }
 
 export type proposalListProps = {
   proposals: any,
+}
+
+type instanceType = {
+  pollName: string,
+  proposals: any[],
+  startTime: string,
+  endTime: string,
+  hasPollStarted: boolean,
+  hasPollEnded: boolean,
+  canVote: boolean,
+  voterCounts: number[],
 }
