79eb68253d52d281aecceee94ad8d31e869ba21e
SamuelRandall <samuelrandall999@gmail.com>
2021-10-21 18:52:45 -0500

    updates createPoll.tsx a bit

--- src/components/CreatePoll.tsx
+++ src/components/CreatePoll.tsx
@@ -77,7 +77,6 @@ export default function CreatePoll() {
       <Typography align={"center"} variant={"h4"}>Create Poll</Typography>
       <form className={classes.root} noValidate autoComplete="off">
         <TextField required id="standard-required" label="Required" defaultValue="Poll Name" value={name} onChange={e => setName(e.target.value)}/>
-
         <MuiPickersUtilsProvider utils={DateFnsUtils}>
           <Typography variant={"h6"}>Start Time:</Typography>
           <Grid container justifyContent="space-around">
@@ -131,14 +130,16 @@ export default function CreatePoll() {
                 'aria-label': 'change time',
               }}
             />
-
           </Grid>
         </MuiPickersUtilsProvider>
       </form>
       <br />
       <Button variant={"contained"} color={"primary"} onClick={handleSubmit}>Submit</Button>
 
+      <p>Poll Address: {pollInstance.address}</p>
       <p>Poll Name: {pollInstance.pollName}</p>
+      <p>Poll Start Time: {pollInstance.startTime}</p>
+      <p>Poll End Time: {pollInstance.endTime}</p>
     </div>
   );
 };
