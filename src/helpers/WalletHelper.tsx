03f91e2f491bb0e8c2c2aa6a90a554e12d3648f5
bighappyface <anomalous20@gmail.com>
2021-10-21 21:49:59 -0400

    Add injected connector and a quick poll deploy script

--- src/helpers/WalletHelper.tsx
+++ src/helpers/WalletHelper.tsx
@@ -1,8 +1,11 @@
 import { WalletConnectConnector } from '@web3-react/walletconnect-connector';
+import { InjectedConnector } from '@web3-react/injected-connector';
+
+export const injected = new InjectedConnector({ supportedChainIds: [1, 3, 4, 5, 42, 1337] });
 
 export const wc = new WalletConnectConnector({
   infuraId: "0168c4f5d973491daae8d12b05e2f051",
-  supportedChainIds: [1, 3, 4, 5, 42],
+  supportedChainIds: [1, 3, 4, 5, 42, 1337],
   qrcode: true,
   pollingInterval: 12000
 });
